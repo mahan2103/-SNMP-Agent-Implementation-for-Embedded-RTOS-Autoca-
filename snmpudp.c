a1841dbf10b773fff9883e824f5c5cba54b1108e
Mahan Hithalamakki Sundaresh <mahan.hs2103@gmail.com>
2026-05-20 13:16:12 +0200

    Create snmpudp.c

---
+++ snmpudp.c
@@ -0,0 +1,23 @@
+8. UDP + PORT 161 (CORE NETWORK)
+🎯 What is happening
+
+SNMP runs on UDP port 161 [arhfoundation.org]
+Manager sends request → Agent listens
+
+
+Example flow:
+PC → UDP 161 → Autoca
+Autoca → reply → PC
+
+
+🧠 Why UDP?
+TCP:
+
+heavy
+slow setup
+
+UDP:
+
+fast ✅
+simple ✅
+perfect for monitoring
